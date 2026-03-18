e423c8ce5a31c3270268f460f047b4f9dc4e6bec
Nikita Grodenchick <nikitaamaxim@gmail.com>
2026-03-18 15:28:37 +0200

    add an item

--- main_file.py
+++ main_file.py
@@ -1,20 +1,48 @@
 from tabulate import tabulate
 
-def inventory_system():
-  inventory = [
+class Inventory():
+  def __init__ (self, id, name, quantity, price):
+    self.id = id
+    self.name = name
+    self.quantity = quantity
+    self.price = price
+
+class InventoryManager():
+         
+  def input_inv (self):
+    inventory = [
     {'id':'764', 'name':"AD", "quantity":2, "price": 60000  },
-    {'id':'74', 'name':"zinq", "quantity":3, "price": 6000  }           
-  ]
-  print (tabulate(inventory, headers="keys", tablefmt="grid"))
+    {'id':'74', 'name':"zinq", "quantity":3, "price": 6000  } 
+    ]
+    print(tabulate(inventory, headers= "keys", tablefmt="grid"))
+
+
+    while True:
+      answer = input('add?')
+      if answer == "yes":
+          id = input("what's the id?")
+          name = input("What is the name")
+          quantity = input("what is the q")
+          price = input("what is the price?")
+
+          Inventory(id, name, quantity, price)
+
+          add_inventory = {
+          "id": id,
+          "name":name,
+          "quantity": quantity,
+          "price": price
+         }
+          inventory.append(add_inventory)
+          print(tabulate(inventory, headers= "keys", tablefmt="grid"))
+
+      else: break
+    
 
 def main():
-  inventory_system()
+  energy = InventoryManager()
+  energy.input_inv()
 
 if __name__ == "__main__":
   main()
 
-
-#for item in inventory:
- #   for key, value in i.items(): print (f"The {key} is {value}")
-
-  #print (f'ID:{item["id"]}| name:{item['name']}|quan: {item["quantity"]}| price: {item["price"]}')
